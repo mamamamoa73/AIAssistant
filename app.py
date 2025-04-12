31bdfbb5ce24346717bce34ac9fc56a3f8463791
mamamamoa73 <mamamamoa73@gmail.com>
2025-04-12 15:32:09 +0300

    Initial commit

--- app.py
+++ app.py
@@ -656,3 +656,8 @@ def analyze_competitors():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+    import os
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
